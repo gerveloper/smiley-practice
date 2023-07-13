991d803aba63325b307246d8be13325e357cdbf7
gerveloper <gerveloper@gmail.com>
2023-07-13 00:07:44 -0300

    Add CSS and JS

--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
 import "./styles.css";
 
 const mouth = document.querySelector(".mouth");
-const f = document.querySelector(".f-off");
+const v = document.querySelector(".vulcan-salute");
 
 function control(e) {
   switch (e.key) {
     case "ArrowUp":
       console.log("Up");
       mouth.className = "";
+      // mouth.classList.remove("sad-mouth");
+      // mouth.classList.remove("appalling-life-mouth");
       mouth.classList.add("happy-mouth");
       break;
 
     case "ArrowDown":
       console.log("Down");
       mouth.className = "";
+      // mouth.classList.remove("happy-mouth");
+      // mouth.classList.remove("appalling-life-mouth");
       mouth.classList.add("sad-mouth");
       break;
 
-    case "f":
-      console.log("f");
-      f.classList.toggle("f-on");
+    case "v":
+      console.log("v");
+      // f.className = "";
+      // mouth.classList.remove("happy-mouth");
+      // mouth.classList.remove("appalling-life-mouth");
+      v.classList.toggle("v-on");
       break;
 
-      case " ":
-        console.log('Space')
-        mouth.className = "";
+    case " ":
+      console.log("Space");
+      mouth.className = "";
       break;
 
     default:
       console.log("Other key");
       mouth.className = "";
+      // mouth.classList.remove("sad-mouth");
+      // mouth.classList.remove("happy-mouth");
       mouth.classList.add("appalling-life-mouth");
   }
 }
